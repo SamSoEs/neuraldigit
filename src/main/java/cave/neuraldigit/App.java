0bcc6a8264840fac73bb4b174c4972b2b88391a5
Saman <samaneghbali1@gmail.com>
2024-11-15 12:49:24 -0500

    Initial Commit

---
+++ src/main/java/cave/neuraldigit/App.java
@@ -0,0 +1,32 @@
+package cave.neuraldigit;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	//inputs 
+        double[] x = {0,1};
+        
+        //weights
+        double[] w = {0.5, 0.5};
+        
+        //bias
+        double b = 0.5;
+        
+        //weighted sum
+        double z = 0.0;
+        
+        for(int i = 0; i < x.length; i++) {
+        	z += x[i] * w[i];
+        }
+        
+        z += b;
+        
+        //activation function
+        double a = z > 0 ? 1.0 : 0.0;
+        
+        //print the activation
+        System.out.println(a);
+    }
+}
